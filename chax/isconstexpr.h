adc4577b2723325052f353b0abb9cfa253d09799
Oderjunkie <>
2023-03-08 19:04:40 +0000

    rename

--- chax/isconstexpr.h
+++ chax/isconstexpr.h
@@ -1,8 +1,8 @@
-#ifndef _chax_isconst
+#ifndef _chax_isconstexpr
   #ifdef __STDC__
     #ifdef __STDC_VERSION__
       #if __STDC_VERSION__ >= 201112L
-        #define _chax_isconst(x) \
+        #define _chax_isconstexpr(x) \
           _Generic((8 ? ((void *)((long)(x) * 0l)) : (int *)8), \
             void *: 0, \
             int *: 1 \
@@ -12,13 +12,14 @@
   #endif
 #endif
 
-#ifndef _chax_isconst
+#ifndef _chax_isconstexpr
   #ifdef __GNUC__
-    #define _chax_isconst(x) \
+    #define _chax_isconstexpr(x) \
       (sizeof(int) == sizeof(*(8 ? ((void *)((long)(x) * 0l)) : (int *)8)))
   #endif
 #endif
 
-#ifndef _chax_isconst
-  #error "unable to define `_chax_isconst`. requires either C11 or later (use of `_Generic`), or GNU C (use of `sizeof(void)`)"
+#ifndef _chax_isconstexpr
+  #error "unable to define `_chax_isconstexpr`. requires either C11 or later \
+(use of `_Generic`), or GNU C (use of `sizeof(void)`)"
 #endif
