8999a72f25d18a04b6d2a5b99d960becea370de3
Oderjunkie <>
2023-04-05 20:17:43 +0000

    make `asmexpr` force the compiler to not get rid of it

--- chax/asmexpr.h
+++ chax/asmexpr.h
@@ -1,12 +1,12 @@
 #ifndef _chax_asmexpr
   #ifdef __cplusplus
-    #define _chax_asmexpr(src) [&](){asm(src); return 0;}()
+    #define _chax_asmexpr(src) [&](){asm volatile(src); return 0;}()
   #endif
 #endif
 
 #ifndef _chax_asmexpr
   #ifdef __GNUC__
-    #define _chax_asmexpr(src) ({__asm__(src); 0;})
+    #define _chax_asmexpr(src) ({asm volatile(src); 0;})
   #endif
 #endif
 
