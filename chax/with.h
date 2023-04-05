08594368afdf206fc8f134b0e9422f3fa75d1536
Oderjunkie <>
2023-04-05 20:15:53 +0000

    allow `with` to accept expressions with commas in them

--- chax/with.h
+++ chax/with.h
@@ -2,14 +2,14 @@
   #ifdef __STDC__
     #ifdef __STDC_VERSION__
       #if __STDC_VERSION__ >= 199901L
-        #define _chax_with(decl) \
+        #define _chax_with(...) \
           for ( \
             int _chax_with_TEMPORARY_VARIABLE = 1; \
             _chax_with_TEMPORARY_VARIABLE; \
             _chax_with_TEMPORARY_VARIABLE = 0 \
           ) \
             for ( \
-              decl; \
+              __VA_ARGS__; \
               _chax_with_TEMPORARY_VARIABLE; \
               _chax_with_TEMPORARY_VARIABLE = 0 \
             )
@@ -19,5 +19,5 @@
 #endif
 
 #ifndef _chax_with
-  #error "unable to define `_chax_with`. requires C99 or later (for loop initial declarations)."
+  #error "unable to define `_chax_with`. requires C99 or later (for loop initial declarations, varaidic macros)."
 #endif
